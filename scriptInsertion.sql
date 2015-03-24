1a04099c32b6092a35e7d01c5ab9c1fb5d1f66bb
GaetanCousin <cousin.gaetan@laposte.net>
2015-03-24 12:28:21 +0100

    Double quote --> Simple quote

--- scriptInsertion.sql
+++ scriptInsertion.sql
@@ -1,37 +1,31 @@
 -- Ingredient
 INSERT INTO INGREDIENT
-VALUES (1, "tomate", "kilo");
+VALUES (1, 'tomate', 'kilo');
 INSERT INTO INGREDIENT
-VALUES (2, "steak", "unite");
+VALUES (2, 'steak', 'unite');
 INSERT INTO INGREDIENT
-VALUES (3, "salade", "kilo");
+VALUES (3, 'salade', 'kilo');
 INSERT INTO INGREDIENT
-VALUES (4, "fromage", "unite");
+VALUES (4, 'fromage', 'unite');
 INSERT INTO INGREDIENT
-VALUES (5, "pomme de terre", "kilo");
+VALUES (5, 'pomme de terre', 'kilo');
 INSERT INTO INGREDIENT
-VALUES (6, "poulet", "kilo");
+VALUES (6, 'poulet', 'kilo');
 
 -- Recipe
 INSERT INTO RECIPE
-VALUES (1, "hamburger1");
+VALUES (1, 'hamburger1');
 INSERT INTO RECIPE
-VALUES (2, "hamburger2");
+VALUES (2, 'hamburger2');
 INSERT INTO RECIPE
-VALUES (3, "hamburger3");
+VALUES (3, 'hamburger3');
 INSERT INTO RECIPE
-VALUES (4, "frite");
+VALUES (4, 'frite');
 INSERT INTO RECIPE
-VALUES (5, "potatoes");
+VALUES (5, 'potatoes');
 
---Menu
+--IngredientRecipe
 
-INSERT INTO MENU (id_menu,name_menu) 
-VALUES (0,"Le starter");
 
-INSERT INTO MENU (id_menu,name_menu)
-VALUES (1,"Le burger");
 
-INSERT INTO MENU (id_menu,name_menu)
-VALUES (2,"La frite");
 
