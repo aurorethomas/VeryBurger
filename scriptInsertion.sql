b45940ee14ca42f93b1fa135287c24ed3eb574ac
GaetanCousin <cousin.gaetan@laposte.net>
2015-03-24 12:14:14 +0100

    ajout initial du fichier scriptInsertion

---
+++ scriptInsertion.sql
@@ -0,0 +1,37 @@
+-- Ingredient
+INSERT INTO INGREDIENT
+VALUES (1, “tomate”, “kilo”);
+INSERT INTO INGREDIENT
+VALUES (2, “steak”, “unite”);
+INSERT INTO INGREDIENT
+VALUES (3, “salade”, “kilo”);
+INSERT INTO INGREDIENT
+VALUES (4, “fromage”, “unite”);
+INSERT INTO INGREDIENT
+VALUES (5, “pomme de terre”, “kilo”);
+INSERT INTO INGREDIENT
+VALUES (6, “poulet”, “kilo”);
+
+-- Recipe
+INSERT INTO RECIPE
+VALUES (1, “hamburger1”);
+INSERT INTO RECIPE
+VALUES (2, “hamburger2”);
+INSERT INTO RECIPE
+VALUES (3, “hamburger3”);
+INSERT INTO RECIPE
+VALUES (4, “frite”);
+INSERT INTO RECIPE
+VALUES (5, “potatoes”);
+
+--Menu
+
+INSERT INTO MENU (id_menu,name_menu) 
+VALUES (0,"Le starter");
+
+INSERT INTO MENU (id_menu,name_menu)
+VALUES (1,"Le burger");
+
+INSERT INTO MENU (id_menu,name_menu)
+VALUES (2,"La frite");
+
