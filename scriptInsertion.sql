1970436592ddb0e4ce9d3d9cf898e5a511808c14
BenjaminBailly <bailly.ben@hotmail.fr>
2015-03-24 12:55:03 +0100

    Ajout ingrédient et recette

--- scriptInsertion.sql
+++ scriptInsertion.sql
@@ -11,6 +11,8 @@ INSERT INTO INGREDIENT
 VALUES (5, 'pomme de terre', 'kilo');
 INSERT INTO INGREDIENT
 VALUES (6, 'poulet', 'kilo');
+INSERT INTO INGREDIENT
+VALUES (7,'Pain','tranche');
 
 -- Recipe
 INSERT INTO RECIPE
@@ -25,9 +27,27 @@ INSERT INTO RECIPE
 VALUES (5, 'potatoes');
 
 --IngredientRecipe
--- des frites 
+--  frites 
 INSERT INTO INGREDIENT RECIPE
 VALUES(5,4,0.4);
+
+--potatoes
+INSERT INTO INGREDIENT RECIPE
+VALUES(5,5,0.4);
+
+--hamburger 3
+
+INSERT INTO INGREDIENT RECIPE
+VALUES(2,3,1);
+
+INSERT INTO INGREDIENT RECIPE
+VALUES(6,3,1);
+
+INSERT INTO INGREDIENT RECIPE
+VALUES(7,3,1);
+
+INSERT INTO INGREDIENT RECIPE
+VALUES(7,3,1);
 
 --Menu
 
@@ -39,7 +59,5 @@ VALUES (1,'Le burger');
 
 INSERT INTO MENU (id_menu,name_menu)
 VALUES (2,'La frite');
-
-
 
 
