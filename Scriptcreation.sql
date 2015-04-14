607973f92dc14dc0d8c3997e759be7a8ba8c2db5
Charles <gastongfaim3@gmail.com>
2015-04-14 11:04:57 +0200

    correction type purchase price

--- Scriptcreation.sql
+++ Scriptcreation.sql
@@ -72,7 +72,7 @@ CREATE TABLE PURCHASE
 	purchaseDate DATE,
 	throwDate DATE,
 	throw_user_id INTEGER,
-	purchasePrice FLOAT,
+	purchasePrice DOUBLE PRECISION,
 	available CHAR(1),
 	id_order INTEGER,
 	id_ingredient INTEGER,
