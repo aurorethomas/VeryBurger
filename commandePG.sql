ac94cf237398993be912f4690db5d5dcc16a195a
BenjaminBailly <bailly.ben@hotmail.fr>
2015-05-05 10:58:58 +0100

    Quelques commandes de test pour PG

---
+++ commandePG.sql
@@ -0,0 +1,11 @@
+--Sans menu
+SELECT id_order,name_recipe
+FROM ORDERS NATURAL JOIN RECIPEORDER NATURAL JOIN RECIPE
+ORDER BY name_recipe DESC
+
+--avec menu
+SELECT id_order,name_recipe
+FROM ORDERS NATURAL JOIN MENUORDER NATURAL JOIN MENU NATURAL JOIN ELEMENTMENU NATURAL JOIN TYPERECIPE NATURAL JOIN RECIPE
+ORDER BY name_recipe DESC
+
+--Vive les crevettes! c'est bon pour le poil de torse!
